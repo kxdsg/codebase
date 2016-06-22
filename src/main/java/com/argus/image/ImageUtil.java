482f2d9af12656b662fde48982a8abfb3b8b44bb
kxdsg <kxdsg@163.com>
2016-06-22 22:05:35 +0800

    add image util

---
+++ src/main/java/com/argus/image/ImageUtil.java
@@ -0,0 +1,71 @@
+package com.argus.image;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.*;
+
+/**
+ * 图片文件传输
+ * Created by xingding on 16/6/22.
+ */
+public class ImageUtil {
+
+
+    /**
+     * 根据图片文件生成对应的base64编码
+     * @param imgFile
+     * @return
+     */
+    public static String encodeImg(String imgFile){
+        InputStream is = null;
+        byte[] data = null;
+        try {
+            is = new FileInputStream(imgFile);
+            data = new byte[is.available()];
+            is.read(data);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(data);
+    }
+
+    /**
+     * 根据base64编码还原成图片文件
+     * @param imgStr
+     * @param destImg
+     */
+    public static void decodeImage(String imgStr, String destImg){
+        if(imgStr == null){
+            return;
+        }
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            byte[] b = decoder.decodeBuffer(imgStr);
+            for(int i=0; i<b.length; i++){
+                if(b[i]<0){
+                    b[i] += 256;
+                }
+            }
+            OutputStream os =  new FileOutputStream(destImg);
+            os.write(b);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String imgStr = ImageUtil.encodeImg("/Users/xingding/Pictures/sun.jpg");
+        System.out.println(imgStr);
+        ImageUtil.decodeImage(imgStr, "/Users/xingding/Pictures/dest.jpg");
+
+
+    }
+
+}
