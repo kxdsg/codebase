6f93dd6b9087c135c42ad8316c2d8187cd43eaa5
kangxd <kxdsg@126.com>
2017-04-09 09:19:26 +0800

    枚举策略

---
+++ src/main/java/com/argus/misc/EnumTest.java
@@ -0,0 +1,13 @@
+package com.argus.misc;
+
+/**
+ * Created by xingding on 2017/4/8.
+ *
+ */
+public class EnumTest {
+
+    public static void main(String[] args) {
+        double pay = PayrollDayEnum.MONDAY.pay(9,50);
+        System.out.println(pay);
+    }
+}
---
+++ src/main/java/com/argus/misc/PayrollDayEnum.java
@@ -0,0 +1,48 @@
+package com.argus.misc;
+
+import sun.security.krb5.internal.PAData;
+
+/**
+ * Created by xingding on 2017/4/8.
+ * 枚举策略
+ */
+public enum PayrollDayEnum {
+    MONDAY(PayTypeEnum.WEEKDAY),
+    TUESDAY(PayTypeEnum.WEEKDAY),
+    WEDNESDAY(PayTypeEnum.WEEKDAY),
+    THURSDAY(PayTypeEnum.WEEKDAY),
+    FRIDAY(PayTypeEnum.WEEKDAY),
+    SATURDAY(PayTypeEnum.WEEKEND),
+    SUNDAY(PayTypeEnum.WEEKEND);
+    private PayTypeEnum payTypeEnum;
+    PayrollDayEnum(PayTypeEnum payTypeEnum){
+        this.payTypeEnum = payTypeEnum;
+    }
+    double pay(double hoursWorked, double payRate){
+        return payTypeEnum.pay(hoursWorked,payRate);
+    }
+
+    private enum PayTypeEnum{
+        WEEKDAY{
+            @Override
+            double overtimePay(double hrs, double payRate) {
+                return hrs <= HOUR_PER_SHIFT?0:(hrs-HOUR_PER_SHIFT)*payRate/2;
+            }
+        },
+        WEEKEND{
+            @Override
+            double overtimePay(double hrs, double payRate) {
+                return hrs*payRate/2;
+            }
+        };
+
+        private static final int HOUR_PER_SHIFT = 8;
+        abstract double overtimePay(double hrs, double payRate);
+
+        double pay(double hoursWorked, double payRate){
+            double basePay = hoursWorked * payRate;
+            return basePay + overtimePay(hoursWorked, payRate);
+        }
+    }
+
+}
