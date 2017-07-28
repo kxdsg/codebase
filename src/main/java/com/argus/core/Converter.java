e222a044f701a568e43fba37848327d75a993088
kangxd <kxdsg@126.com>
2017-07-28 09:37:12 +0800

    convert

---
+++ src/main/java/com/argus/core/Converter.java
@@ -0,0 +1,13 @@
+package com.argus.core;
+
+/**
+ * @author xingding
+ * @date 2017/7/26.
+ */
+public class Converter {
+
+    public static void main(String[] args) {
+        //转二进制
+        System.out.println(Integer.toBinaryString(16));
+    }
+}
