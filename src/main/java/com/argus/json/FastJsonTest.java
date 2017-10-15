ee7ae42cf8e6a189d7d69c2e2639f0f0f25c81e1
kangxd <kxdsg@126.com>
2017-10-15 14:22:09 +0800

    test

--- src/main/java/com/argus/json/FastJsonTest.java
+++ src/main/java/com/argus/json/FastJsonTest.java
@@ -4,6 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  fastjson解析测试类
  * Created by xingding on 2016/9/5.
@@ -49,6 +52,13 @@ public class FastJsonTest {
      */
     public static void main(String[] args) {
 
+        //map to josn
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","kang");
+        map.put("age",100);
+        System.out.println(JSON.toJSONString(map));
+
+        //parse json string
         String jsonStr = "{\"errCode\":\"000000\",\"msg\":\"success\",\"data\":{\"certificateNo\":\"1234213\",\"name\":\"李逵\",\"organization\":\"会计师事务所有限公司\",\"approvalNo\":\"协字（1998）84号\",\"approvalDate\":\"1998-11-30\",\"approach\":\"考试\",\"lastAnnualDate\":\"2007-04-30\",\"list\":[{\"orgName\":\"会计师事务所有限公司\",\"certificateNo\":\"41210069\",\"punishType\":\"11\",\"punishDate\":\"\"}]}}";
         //获取errCode
         JSONObject rootObj = JSON.parseObject(jsonStr);
