c120daeececfddc0a90ca2926e5534ab6bfd5409
kangxd <kxdsg@126.com>
2017-12-30 22:02:26 +0800

    add

---
+++ src/main/java/com/argus/common/Constants.java
@@ -0,0 +1,30 @@
+package com.argus.common;
+
+/**
+ *
+ */
+public class Constants {
+
+    /**
+     * 通用成功返回码
+     */
+    public final static String RTN_CODE_SUCCESS = "000000";
+
+    /**
+     * 通用成功信息
+     */
+    public final static String RTN_MESSAGE_SUCCESS = "success";
+
+    /**
+     * 通用错误返回码
+     */
+    public final static String RTN_CODE_ERROR = "999999";
+
+    /**
+     * 通用错误信息
+     */
+    public final static String RTN_MESSAGE_ERROR = "请求发生异常";
+
+
+
+}
