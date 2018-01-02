d1a216ee7c052b5f83989b88d6c84fe5d496e7a9
kangxd <kxdsg@126.com>
2018-01-02 09:36:05 +0800

    枚举测试

--- src/main/java/com/argus/enummj/EnumTest.java
+++ src/main/java/com/argus/enummj/EnumTest.java
@@ -7,7 +7,24 @@ package com.argus.enummj;
 public class EnumTest {
 
     public static void main(String[] args) {
-        double pay = PayrollDayEnum.MONDAY.pay(9,50);
-        System.out.println(pay);
+//        double pay = PayrollDayEnum.MONDAY.pay(9,50);
+//        System.out.println(pay);
+        WeekDay weekDay = WeekDay.FRI;
+        System.out.println(weekDay);       //FRI枚举
+        System.out.println(weekDay.name()); //FRI字符串
+        System.out.println(weekDay.ordinal()); //5,从0开始
+        System.out.println(WeekDay.valueOf("SUN")); //SUN枚举
+        System.out.println(WeekDay.SUN.equals(WeekDay.valueOf("SUN"))); //true
+        System.out.println("FRI".equals(WeekDay.FRI)); //false
+        System.out.println("FRI".equals(WeekDay.FRI.name())); //true 字符串比较
+        System.out.println("FRI".equals(WeekDay.FRI.toString())); //true 字符串比较
+        System.out.println(WeekDay.values());
     }
+
+    public enum WeekDay{
+        SUN,MON,TUE,WED,THI,FRI,SAT;
+    }
+
+
+
 }
